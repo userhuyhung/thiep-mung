e49cf7dd016d6cdbb5d5aa14725183326fd20d4a
userhuyhung <89526306+userhuyhung@users.noreply.github.com>
2021-10-21 01:58:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Hiểu';
+const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Hello Understand';
-const contentCard = 'Chúc Anh hiểu sớm trị được căn bệnh hói đầu';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
